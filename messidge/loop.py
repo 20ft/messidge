ff700cf23af7eb50814f4790fe4e38908ae3e2e1
David Preece <davep@zedkep.com>
2017-10-31 19:37:07 +1300

    Long polls were causing annoying shutdown delays

--- messidge/loop.py
+++ messidge/loop.py
@@ -61,7 +61,7 @@ class Loop:
                         logging.warning("Event loop stalled for (ms): " + str(latency))
 
                 # fetch the events
-                events = self.p.poll(timeout=5000)
+                events = self.p.poll(timeout=500)
                 tmr = time.time()
                 msg = None
 
