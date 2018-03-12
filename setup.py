8e5ef93ca56c2bdc9d8fdcb9061f68f53b9e20fa
David Preece <davep@zedkep.com>
2018-03-12 18:24:35 +1300

    Version 1.2.2

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import setup
 
 setup(
     name='messidge',
-    version='1.2.1',
+    version='1.2.2',
     author='David Preece',
     author_email='davep@20ft.nz',
     url='https://20ft.nz',
